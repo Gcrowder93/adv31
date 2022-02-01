3bdf24f62dffb503fde0b18c878eed4f53a93650
Chase Crowder <gcrowder93@gmail.com>
2022-01-31 17:04:18 -0800

    added some folders, and a couple basic functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomeView from './Views/HomeView';
+// import MonsterList from './Components/MonsterList/MonsterList';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <HomeView />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/services/Monsters.js
+++ src/services/Monsters.js
@@ -1,5 +1,5 @@
-// export async function getMonsters() {
-//   const response = await fetch(`https://botw-compendium.herokuapp.com/api/v2/category/monsters`);
-//   const monsterData = await response.json();
-//   return monsterData;
-// }
+export async function getMonsters() {
+  const response = await fetch(`https://botw-compendium.herokuapp.com/api/v2/category/monsters`);
+  const monsterData = await response.json();
+  return monsterData;
+}
