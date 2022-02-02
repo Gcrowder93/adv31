1ecf207ed018d91b35a43062e167286cfcc877ea
Chase Crowder <gcrowder93@gmail.com>
2022-02-01 17:18:23 -0800

    was going to start testing, but I dont feel confident and the end of the day is approaching

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
+import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import HomeView from './Views/HomeView';
 
 test.skip('renders learn react link', () => {
   render(<App />);
--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import MonsterList from '../Components/MonsterList/MonsterList';
 
 export default function HomeView() {
   return (
     <div>
-      <h1>This is a compendium for Breath of the Wild Monsters.</h1>
+      <header>This is a compendium for Breath of the Wild Monsters.</header>
       <h2>Click the link below to view a list of all Monsters in BOTW.</h2>
-      <Link to={'MonsterList'}>Monster List</Link>
+      <a href="/MonsterList">Monster List</a>
+      {/* <Link to={'MonsterList'}>Monster List</Link> */}
       <h3>Click on an image to learn more about that monster.</h3>
       <h4>Things like its Common Location, what it Drops, etc.</h4>
     </div>
