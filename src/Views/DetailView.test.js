a77c888a9438e8f54e4cf03c6c1bd9a61a3f3b0c
Chase Crowder <gcrowder93@gmail.com>
2022-02-04 17:57:30 -0800

    fixed test

--- src/Views/DetailView.test.js
+++ src/Views/DetailView.test.js
@@ -1,35 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import { Route, MemoryRouter } from 'react-router-dom';
-import MonsterDetails from '../Components/MonsterDetails/MonsterDetails';
-// import DetailView from './DetailView';
+// import MonsterDetails from '../Components/MonsterDetails/MonsterDetails';
+import DetailView from './DetailView';
 
-const mockmonsters = {
-  category: 'monsters',
-  common_locations: ['Hyrule Castle'],
-  description:
-    'The Sheikah of ancient Hyrule developed this as a weapon to combat Ganon. This stationary model was used for defending important structures. Its offensive power is on par with other Guardians, and omitting the legs kept the manufacturing costs low.',
-  drops: [
-    'ancient screw',
-    'ancient spring',
-    'ancient gear',
-    'ancient shaft',
-    'ancient core',
-    'giant ancient core',
-  ],
-  id: 127,
-  image: 'https://botw-compendium.herokuapp.com/api/v2/entry/guardian_turret/image',
-  name: 'guardian turret',
-};
+// const mockmonsters = {
+//   category: 'monsters',
+//   common_locations: ['Hyrule Castle'],
+//   description:
+//     'The Sheikah of ancient Hyrule developed this as a weapon to combat Ganon. This stationary model was used for defending important structures. Its offensive power is on par with other Guardians, and omitting the legs kept the manufacturing costs low.',
+//   drops: [
+//     'ancient screw',
+//     'ancient spring',
+//     'ancient gear',
+//     'ancient shaft',
+//     'ancient core',
+//     'giant ancient core',
+//   ],
+//   id: 127,
+//   image: 'https://botw-compendium.herokuapp.com/api/v2/entry/guardian_turret/image',
+//   name: 'guardian turret',
+// };
 test('Detail View Page renders specific monster image and details', async () => {
   render(
-    <MemoryRouter initialEntries={['/detailview']}>
-      <Route path="/detailview">
-        <MonsterDetails monsters={mockmonsters} />
+    <MemoryRouter initialEntries={['/detailview/127']}>
+      <Route path="/DetailView/:id">
+        <DetailView />
       </Route>
+      {/* <Route path="/detailview">
+        <MonsterDetails monsters={mockmonsters} />
+      </Route> */}
       );
     </MemoryRouter>
   );
 
-  const image = await screen.findAllByRole('img');
-  expect(image).toHaveLength(2);
+  const image = await screen.findByText(/guardian turret/i);
+  expect(image).toBeInTheDocument();
 });
